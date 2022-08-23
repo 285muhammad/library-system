bbe187ed8834d4f599f75cf16a450122a0514812
285muhammad <285muhammad@gmail.com>
2022-08-23 12:38:31 +0200

    define book struct and its vars and constructor

--- system.cpp
+++ system.cpp
@@ -7,6 +7,20 @@ using namespace std;
 const int MAX_BOOKS (10);
 const int MAX_USERS (10);
 
+struct book
+{
+    string name;
+    int id;
+    int total_quantity;
+    int total_borrowed;
+
+    book(){
+        name = " ";
+        id = -1 ;
+        total_borrowed = total_quantity = 0 ;
+    }
+};
+
 
 int main(){
     return 0;
